f4d82dd7d8ad5ef21136470a37495c0b6e89062d
mjm114514 <956697547@qq.com>
2021-01-11 23:52:05 +0800

    Initialize validation layers.

--- vulkan-triangle/HelloTriangleApplication.h
+++ vulkan-triangle/HelloTriangleApplication.h
@@ -1,25 +1,18 @@
-#ifndef HELLO_TRIANGLE_APPLICATION_H__
-#define HELLO_TRIANGLE_APPLICATION_H__
+#ifndef __HELLO_TRIANGLE_APPLICATION_H__
+#define __HELLO_TRIANGLE_APPLICATION_H__
 
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #define GLFW_INCLUDE_VULKAN
 #include <stdexcept>
+#include <iostream>
 #include <vector>
-
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-
-std::vector<const char*> validationLayers = {
-	"VK_LAYER_KHRONOS_validation"
-};
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
 #else
 const bool enableValidationLayers = true;
 #endif // NDEBUG
-
 
 class HelloTriangleApplication {
 public:
@@ -38,35 +31,11 @@ private:
 
 	bool checkValidationLayerSupport();
 
-	void pickPhysicalDevice();
-
-	bool isDeviceSuitable(VkPhysicalDevice device);
-
 	std::vector<const char*> getRequiredExtensions();
 
 	void setupDebugMessenger();
 
 	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-
-	static VkResult CreateDebugUtilsMessengerEXT(
-		VkInstance instance,
-		const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
-		const VkAllocationCallbacks* pAllocator,
-		VkDebugUtilsMessengerEXT* pDebugMessenger
-	);	
-
-	static void DestroyDebugUtilsMessengerEXT(
-		VkInstance instance,
-		VkDebugUtilsMessengerEXT debugMessenger,
-		const VkAllocationCallbacks* pAllocator
-	);
-
-	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-		VkDebugUtilsMessageTypeFlagsEXT messageType,
-		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-		void* pUserData
-	);
 
 private:
 	GLFWwindow* window;
